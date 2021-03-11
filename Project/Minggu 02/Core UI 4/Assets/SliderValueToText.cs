a69ae03fb83a1296e0f5b0a57f3d7a619031780d
PanduDLaksono <54766392+PanduDLaksono@users.noreply.github.com>
2021-03-11 21:34:18 +0700

    up project 2

---
+++ Project/Minggu 02/Core UI 4/Assets/SliderValueToText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderValueToText : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        ShowSliderValue();
+    }
+
+    public void ShowSliderValue()
+    {
+        string sliderMessage = "Slider Value = " + sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
+}
