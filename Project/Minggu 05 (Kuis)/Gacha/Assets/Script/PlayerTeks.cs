300041f47487749e799934140c7985e18b347de7
PanduDLaksono <54766392+PanduDLaksono@users.noreply.github.com>
2021-03-14 21:36:19 +0700

    up project kuis 1

---
+++ Project/Minggu 05 (Kuis)/Gacha/Assets/Script/PlayerTeks.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerTeks : MonoBehaviour
+{
+    public Text sqText;
+    private int totalSq = 0;
+    void Sqt()
+    {
+        UpdateSqText();
+    }
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("sq"))
+        {
+            totalSq++;
+            UpdateSqText();
+            Destroy(hit.gameObject);
+        }
+    }
+    private void UpdateSqText()
+    {
+        string sqMessage = "SQ = " + totalSq;
+        sqText.text = sqMessage;
+    }
+}
