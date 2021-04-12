8013b0b371d8f15b3a93cac8a604cc7f058f1164
PanduDLaksono <54766392+PanduDLaksono@users.noreply.github.com>
2021-04-12 21:04:52 +0700

    up praktikum modul 9

---
+++ Project/Minggu 09/Tugas/Assets/Script/RotateLight.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public class RotateLight : MonoBehaviour
+{
+    public float speed = -1.0f;
+    void Update()
+    {
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
+    }
+}
