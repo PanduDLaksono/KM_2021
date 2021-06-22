a6c58ac3f97c5dcee68fb5f92db1402ad33b959c
PanduDLaksono <54766392+PanduDLaksono@users.noreply.github.com>
2021-06-22 21:08:57 +0700

    up uas

---
+++ Project/Minggu 16 (UAS)/Shooting Stand/Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame1()
+    {
+        Application.LoadLevel("Level1");
+    }
+
+    public void PlayGame2()
+    {
+        Application.LoadLevel("Level2");
+    }
+
+    public void QuitGame() 
+    {
+        Debug.Log("Keluar game!!!");
+        Application.Quit();
+    }
+
+
+}
